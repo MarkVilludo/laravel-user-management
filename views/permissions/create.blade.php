5d98efcfd19b38f83035a3aff0edfb4df82ce91f
Mark Villudo <markanthony.villudo@gmail.com>
2018-09-05 13:18:27 +0800

    Update view create permissions additional module fields

--- views/permissions/create.blade.php
+++ views/permissions/create.blade.php
@@ -4,37 +4,39 @@
 
 @section('content')
 
+ <!-- //Main content page. -->
+
 <div class='col-lg-4 col-lg-offset-4'>
 
-{{-- @include ('errors.list') --}}
+    {{-- @include ('errors.list') --}}
 
-{{ Form::open(array('url' => 'permissions')) }}
+    {{ Form::open(array('url' => 'permissions')) }}
 
-<div class="form-group">
-    {{ Form::label('name', 'Name') }}
-    {{ Form::text('name', '', array('class' => 'form-control')) }}
-</div>
-<br>
-<div class="form-group">
-    {{ Form::label('module', 'Module Name') }}
-    {{ Form::text('module', '', array('class' => 'form-control')) }}
-</div>
-<br>
-<h5><b>Assign Permission to Roles</b></h5>
-    <div class="row">
-        <div class='col-lg-6 form-group'>
-            @foreach ($roles as $role)
-                {{ Form::checkbox('roles[]',  $role->id) }}
-                {{ Form::label($role->name, ucfirst($role->name)) }}<br>
-
-            @endforeach
-        </div>
+    <div class="form-group">
+        {{ Form::label('name', 'Name') }}
+        {{ Form::text('name', '', array('class' => 'form-control')) }}
     </div>
-<br>
-{{ Form::submit('Add', array('class' => 'btn btn-primary')) }}
+    <br>
+    <div class="form-group">
+        {{ Form::label('module', 'Module Name') }}
+        {{ Form::text('module', '', array('class' => 'form-control')) }}
+    </div>
+    <br>
+    <h5><b>Assign Permission to Roles</b></h5>
+        <div class="row">
+            <div class='col-lg-6 form-group'>
+                @foreach ($roles as $role)
+                    {{ Form::checkbox('roles[]',  $role->id) }}
+                    {{ Form::label($role->name, ucfirst($role->name)) }}<br>
 
-{{ Form::close() }}
+                @endforeach
+            </div>
+        </div>
+    <br>
+    {{ Form::submit('Add', array('class' => 'btn btn-primary')) }}
+
+    {{ Form::close() }}
 
 </div>
-
+<!-- End main content page -->
 @endsection
