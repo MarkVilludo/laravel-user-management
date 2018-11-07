e8c4cf6c281c3d7728c58d531cdb828b2b7f3187
Mark Villudo <mark.villudo@synergy88digital.com>
2018-11-07 11:31:43 +0800

    Updates user ma

--- views/includes/header.blade.php
+++ views/includes/header.blade.php
@@ -8,7 +8,6 @@
     <meta name="author" content="Coderthemes">
     <meta name="description" content="Trivia Form">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <script src="{{asset("assets/js/modernizr.min.js")}}"></script>
     <title>Trivia Form</title>
     @yield('top-styles')
 <!--     <link rel="shortcut icon" href="assets/images/favicon.ico"> -->
